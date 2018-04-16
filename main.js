b8031cdcd8bb3a38008c14e79dc5472f7ea41b8a
Patrick van Marsbergen <patrick@vanmarsbergen.com>
2018-04-16 14:53:48 +0200

    Replaced tasks with validator

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.getElementById("code-editor").addEventListener("keyup", function (event
   this.style.height = this.scrollHeight + 'px';
   document.getElementsByClassName("preview")[0].innerHTML = event.target.value;
 
-  checkTasks(event.target.value);
+  validateHTML(event.target.value);
 });
 document.getElementsByClassName("preview")[0].innerHTML = document.getElementById("code-editor").value;
 
@@ -14,50 +14,58 @@ function debug() {
   document.getElementsByTagName("body")[0].className = document.getElementsByTagName("body")[0].className.indexOf('debug') > -1 ? '' : 'debug';
 }
 
-var tasks = [
-  '<p>Now I understand HTML.</p>',
-  '<p>Now I understand HTML. And I love it!</p>',
-  ['<p>Now I understand HTML.<br />And I love it!</p>', '<p>Now I understand HTML.<br>And I love it!</p>'],
-];
-
-var currentTask = 0;
-
-window.setCurrentTask = function(i) {
-  currentTask = i;
-};
-
-function checkTasks(code) {
-  console.log('Current task: ', currentTask);
-
-  if (!tasks[currentTask]) {
-    return;
-  }
-
-  if (!Array.isArray(tasks[currentTask])) {
-    if (code.replace(/\s/gi, '').toLowerCase() === tasks[currentTask].replace(/\s/gi, '').toLowerCase()) {
-      taskGood();
-    }
-    return;
-  }
-
-  for (var i = 0; i < tasks[currentTask].length; i++) {
-    console.log(code.replace(/\s/gi, '').toLowerCase(), tasks[currentTask][i].replace(/\s/gi, '').toLowerCase());
-    if (code.replace(/\s/gi, '').toLowerCase() === tasks[currentTask][i].replace(/\s/gi, '').toLowerCase()) {
-      taskGood();
-      return;
-    }
-  }
+function validateHTML(html) {
+  var doc = document.createElement('div');
+  doc.innerHTML = html;
+  document.getElementById("validation-status").innerHTML = (doc.innerHTML === html.replace(/<br\s*\/>/gi, '<br>')) ? 'HTML Validation: &check;' : 'HTML Validation: &cross;';
 }
 
-function taskGood() {
-  currentTask++;
-
-  var taskSuccess = document.createElement('div');
-  taskSuccess.className = 'taskSuccess';
-  taskSuccess.id = 'taskSuccess';
-  document.body.appendChild(taskSuccess);
-
-  window.setTimeout(function () {
-    document.getElementById("taskSuccess").remove();
-  }, 4000);
-}
+// var tasks = [
+//   /<p>nowiunderstandhtml\.<\/p>/gi,
+//   /<p>nowiunderstandhtml\.andiloveit!<\/p>/gi,
+//   /<p>nowiunderstandhtml\.<br\/*>andiloveit!<\/p>/gi,
+//   /<p>nowiunderstandhtml\.<br\/*>andi<b|strong>love<\/b|strong>it!<\/p>/gi,
+//   /(<p>nowiunderstandhtml\.<br\/*><em|i>andi<b|strong>love<\/b|strong><\/em|i>it!<\/p>)/gi
+// ];
+//
+// var currentTask = 3;
+//
+// window.setCurrentTask = function(i) {
+//   currentTask = i;
+// };
+//
+// function checkTasks(code) {
+//   console.log('Current task: ', currentTask);
+//
+//   if (!tasks[currentTask]) {
+//     return;
+//   }
+//
+//   if (!Array.isArray(tasks[currentTask])) {
+//     console.log(code.replace(/\s/gi, '').toLowerCase());
+//     if (code.replace(/\s/gi, '').toLowerCase().match(tasks[currentTask])) {
+//       taskGood();
+//     }
+//     return;
+//   }
+//
+//   for (var i = 0; i < tasks[currentTask].length; i++) {
+//     if (code.replace(/\s/gi, '').toLowerCase().match(tasks[currentTask][i])) {
+//       taskGood();
+//       return;
+//     }
+//   }
+// }
+//
+// function taskGood() {
+//   currentTask++;
+//
+//   var taskSuccess = document.createElement('div');
+//   taskSuccess.className = 'taskSuccess';
+//   taskSuccess.id = 'taskSuccess';
+//   document.body.appendChild(taskSuccess);
+//
+//   window.setTimeout(function () {
+//     document.getElementById("taskSuccess").remove();
+//   }, 4000);
+// }
